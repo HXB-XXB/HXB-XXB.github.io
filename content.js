08b66356ada3557a13af9707cf82ec96c2e980c2
xxb <13922452769@163.com>
2025-08-01 19:40:23 +0800

    Add files via upload

--- content.js
+++ content.js
@@ -1,9 +1,66 @@
 const themeToggle = document.getElementById('themeToggle');
 const body = document.body;
 
+let mouse = {
+    x: 0,
+    y: 0
+}
+
 setTimeout(() => {
     document.querySelector(".curtain").remove();
 }, 2000);
+
+function addWorks(url, name, tags) {
+    let ul = document.getElementById("worksList");
+    let li = document.createElement("li");
+
+    function addTags(className, txt) {
+        let tag = document.createElement("div");
+        tag.className = className;
+        let span = document.createElement("span");
+        span.style.transition = "all 0.3s ease";
+        span.style.opacity = "0";
+        span.textContent = txt;
+        tag.appendChild(span);
+        li.appendChild(tag);
+        tag.addEventListener("mouseenter", () => {
+            tag.style.width = `${span.offsetWidth}px`;
+            span.style.opacity = "1";
+            tag.style.padding = "5px";
+            tag.style.margin = "0";
+        });
+        tag.addEventListener("mouseleave", () => {
+            tag.style.width = `20px`;
+            span.style.opacity = "0";
+            tag.style.padding = "0";
+            tag.style.margin = "5px";
+        });
+    }
+    if(tags.web) addTags("tagWeb", "浏览器拓展");
+    if(tags.js) addTags("tagJs", "JavaScript");
+    if(tags.html) addTags("tagHtml", "HTML");
+    if(tags.cpp) addTags("tagCpp", "C++");
+    if(tags.cp) addTags("tagCp", "C#");
+    if(tags.c) addTags("tagC", "C");
+    if(tags.other) addTags("tagOther", "其他");
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.style.marginLeft = "10px";
+    link.textContent = name;
+    li.appendChild(link);
+    ul.appendChild(li);
+}
+
+addWorks("https://github.com/HXB-XXB/furryDoll", "毛绒玩偶/兽设展示玩偶", {
+    web: true,
+    js: true,
+    html: false,
+    cpp: false,
+    cp: false,
+    c: false,
+    other: false,
+});
 
 function setTheme(mode) {
     if(mode === "light") {
@@ -11,13 +68,16 @@ function setTheme(mode) {
         body.classList.add('lightMode');
         themeToggle.querySelector("span").textContent = '切换暗色模式';
         themeToggle.querySelector("div").className = 'sun';
-        // document.getElementById("anotherMark").src = "icon/dark/github.svg";
+        document.getElementById("anotherMark").src = "icon/dark/github.svg";
         document.getElementById("emailMark").src = "icon/dark/email.svg";
         document.getElementById("githubMark").src = "icon/dark/github.svg";
         document.getElementById("githubMark2").src = "icon/dark/github.svg";
         document.getElementById("wechatMark").src = "icon/dark/wechat.svg";
-        document.querySelectorAll(".emoji").forEach((e) => {
+        document.querySelectorAll(".emojiBlack").forEach((e) => {
             e.style.setProperty('--inv', `0%`);
+        })
+        document.querySelectorAll(".emojiWhite").forEach((e) => {
+            e.style.setProperty('--inv', `100%`);
         })
         localStorage.setItem('theme', 'light');
     }
@@ -26,13 +86,16 @@ function setTheme(mode) {
         body.classList.add('darkMode');
         themeToggle.querySelector("span").textContent = '切换亮色模式';
         themeToggle.querySelector("div").className = 'moon';
-        // document.getElementById("anotherMark").src = "icon/light/github.svg";
+        document.getElementById("anotherMark").src = "icon/light/github.svg";
         document.getElementById("emailMark").src = "icon/light/email.svg";
         document.getElementById("githubMark").src = "icon/light/github.svg";
         document.getElementById("githubMark2").src = "icon/light/github.svg";
         document.getElementById("wechatMark").src = "icon/light/wechat.svg";
-        document.querySelectorAll(".emoji").forEach((e) => {
+        document.querySelectorAll(".emojiBlack").forEach((e) => {
             e.style.setProperty('--inv', `100%`);
+        })
+        document.querySelectorAll(".emojiWhite").forEach((e) => {
+            e.style.setProperty('--inv', `0%`);
         })
         localStorage.setItem('theme', 'dark');
     }
@@ -85,3 +148,40 @@ function showTips(txt) {
         }, 500);
     }, 3000);
 }
+
+document.addEventListener("mousemove", (e) => {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+})
+
+document.querySelectorAll("section").forEach((e) => {
+    e.addEventListener("click", () => {
+        let div = document.createElement("div");
+        div.className = "curtainDiv";
+        e.appendChild(div);
+        let curtain = document.createElement("div");
+        curtain.className = "curtainRound";
+        curtain.style.left = `${mouse.x - e.offsetLeft + window.scrollX - 20}px`;
+        curtain.style.top = `${mouse.y - e.offsetTop + window.scrollY - e.offsetHeight + 20}px`;
+        div.insertBefore(curtain, div.firstChild);
+        setTimeout(() => {
+            div.remove();
+        }, 2000)
+    })
+})
+document.querySelectorAll("button").forEach((e) => {
+    e.addEventListener("click", () => {
+        let div = document.createElement("div");
+        div.className = "curtainDiv";
+        e.appendChild(div);
+        let curtain = document.createElement("div");
+        curtain.className = "curtainRound";
+        curtain.style.left = `${mouse.x - e.offsetLeft - 20}px`;
+        curtain.style.top = `${mouse.y - e.offsetTop - 25}px`;
+        curtain.style.backgroundColor = "#414141";
+        div.insertBefore(curtain, div.firstChild);
+        setTimeout(() => {
+            div.remove();
+        }, 2000)
+    })
+})
